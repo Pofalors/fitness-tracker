15dafe531b87c0f9f75e10fd7dd77bc73c4d6da5
Pofalors <spanos560@gmail.com>
2026-02-27 00:54:40 +0100

    almost fixed dark mode

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -51,7 +51,7 @@ export const Dashboard = () => {
                     onClick={logout}
                     className="bg-white/20 hover:bg-white/30 px-4 py-2 rounded-full text-sm font-medium transition-colors"
                   >
-                    Αποσύνδεση
+                    {t('logout')}
                   </button>
                 </>
               )}
